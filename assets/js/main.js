b3a11e19a13fb5f5ad193f7b6819c86d46e02b4d
broyettore <broyettore@gmail.com>
2023-02-23 17:05:58 +0100

    Project done with bonuses

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,40 @@
+const divContainer = document.querySelector(".container")  // main container selected
+const startBtn = document.querySelector(".action") // button start selected
 
 
- for (let i = 1; i <= 100; i++) {
-   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("Fizzbuzz");
-   } else if (i % 3 === 0) {
-    console.log("Fizz");
-   } else if (i % 5 === 0) {
-    console.log("Buzz");
-   } else {
-    console.log(i)
-   }
- }
+// EVENT TO MAKE GAME APPEAR
+
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+
+    for (let i = 1; i <= 100; i++) {
+
+        // Create a div inside main container with a class of num-box 
+        const divChild = document.createElement("div");
+        divChild.classList.add("num-box");
+        divContainer.append(divChild);
+
+        // Create a span inside divChild with a class of num
+        const divSpan = document.createElement("span");
+        divSpan.classList.add("num");
+        divChild.append(divSpan);
+ 
+        //   LOOP STARTS 
+       if (i % 3 === 0 && i % 5 === 0) {
+        divChild.classList.add("fizzbuzz");
+        divSpan.append("FizzBuzz");
+       } else if (i % 3 === 0) {
+        divChild.classList.add("fizz");
+        divSpan.append("Fizz");
+       } else if (i % 5 === 0) {
+        divChild.classList.add("buzz");
+        divSpan.append("Buzz");
+       } else {
+        divSpan.append(i);
+       }
+     }
+    
+}
+
+
